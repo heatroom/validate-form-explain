7032bb4194239530b12e1dec47ff75685b7bb857
foriacus <634844533@qq.com>
2014-04-02 10:51:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var classes = require('classes');
+
+/**
+ * Expose `plugin`.
+ */
+
+module.exports = plugin;
+
+
+/**
+ * Apply a plugin to the message adapters.
+ *
+ * @param {Validator} validator
+ */
+
+function plugin (validator) {
+
+	var adapter = validator.adapter;
+  var el = adapter.el;
+  var valid = adapter.valid;
+  var invalid = adapter.invalid;
+  var clear = adapter.clear;
+
+  validator.valid(function (view) {
+    valid.apply(adapter, arguments);
+    var input = el(view);
+    classes(input).remove('error');
+  });
+
+  /**
+   * When invalid, add class `error`.
+   *
+   * @param {Element} el
+   * @param {String} message
+   */
+
+  validator.invalid(function (view) {
+  	invalid.apply(adapter, arguments);
+  	var input = el(view);
+    classes(input).add('error');
+  });
+
+};
